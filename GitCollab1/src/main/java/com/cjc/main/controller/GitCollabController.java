ab62c675b895062499fd9116761dffd5844f9819
nitin-dotcom <ni3shirsath@gmail.com>
2024-07-02 15:37:48 +0530

    update method commited by Nitin

--- GitCollab1/src/main/java/com/cjc/main/controller/GitCollabController.java
+++ GitCollab1/src/main/java/com/cjc/main/controller/GitCollabController.java
@@ -3,6 +3,7 @@ package com.cjc.main.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cjc.main.model.Student;
@@ -20,5 +21,11 @@ public class GitCollabController {
 	{
 		return null;
 	}
+	@PutMapping("/update")
+	
+		public ResponseEntity<Student> updateStudent()
+		{
+			return null;
+		}
+	}
 
-}
