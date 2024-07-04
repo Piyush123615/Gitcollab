21e147d80a1991149480bb086480d212903bbaa3
nitin-dotcom <ni3shirsath@gmail.com>
2024-07-04 16:21:56 +0530

    put method by Nitin

--- GitCollab1/src/main/java/com/cjc/main/controller/GitCollabController.java
+++ GitCollab1/src/main/java/com/cjc/main/controller/GitCollabController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,9 +16,11 @@ import com.cjc.main.model.Student;
 import com.cjc.main.service.GitCollabService;
 
 @RestController
-public class GitCollabController {
+public class GitCollabController 
+{
 	
 	@Autowired GitCollabService service;
+
 
 	@PostMapping("/createstudent")
 	public ResponseEntity<Student>createstudent(@RequestBody Student s){
@@ -32,5 +35,13 @@ public class GitCollabController {
 		  return null;
 	  }
 
+
+	
+	@PutMapping("/updateData")
+	public String updateDataUser(@PathVariable Student stu )
+	{
+		return null;
+	}
+	
 	}
 
