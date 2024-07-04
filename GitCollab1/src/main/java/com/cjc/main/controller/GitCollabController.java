311271f746dabef55d83e2b4b9ef7333879e79be
rampipalwa197 <rampipalwa123@gmail.com>
2024-07-04 16:19:24 +0530

    stash push by ram

--- GitCollab1/src/main/java/com/cjc/main/controller/GitCollabController.java
+++ GitCollab1/src/main/java/com/cjc/main/controller/GitCollabController.java
@@ -1,5 +1,7 @@
 package com.cjc.main.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +18,19 @@ import com.cjc.main.service.GitCollabService;
 public class GitCollabController {
 	
 	@Autowired GitCollabService service;
+
 	@PostMapping("/createstudent")
 	public ResponseEntity<Student>createstudent(@RequestBody Student s){
 		
 		return null;
 	}
+
+	
+	  @GetMapping("/getStudent")
+	  public ResponseEntity<List<Student>> getAllStudent()
+	  {
+		  return null;
+	  }
+
 	}
 
